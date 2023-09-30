707b2340bf3c26aa1a1fda050846f6899c953fba
OHMelin <oliverholstmelin@gmail.com>
2023-09-30 20:37:38 +0200

    SEO
    - Added react-helmet package
    - Added open graph
    - Added banner to open graph

--- src/App.js
+++ src/App.js
@@ -4,10 +4,29 @@ import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
+import Banner from './assets/banner.jpg'
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div>
+      <Helmet>
+        <title>OHMelin's Portfolio</title>
+        <meta name="description" content="Visit my site to view my projects and read more about me!" />
+
+        <meta property="og:url" content="https://ohmelin.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="OHMelin's Portfolio" />
+        <meta property="og:description" content="Visit my site to view my projects and read more about me!" />
+        <meta property="og:image" content={Banner} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="ohmelin.com" />
+        <meta property="twitter:url" content="https://ohmelin.com/" />
+        <meta name="twitter:title" content="OHMelin's Portfolio" />
+        <meta name="twitter:description" content="Visit my site to view my projects and read more about me!" />
+        <meta name="twitter:image" content={Banner} />
+      </Helmet>
       <Hero />
       <Experience />
       <Projects />
