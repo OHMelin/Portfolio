20da256b160531c8193d76ac3f191cd833086e4e
OHMelin <oliverholstmelin@gmail.com>
2023-10-10 09:19:12 +0200

    Fixed footer tooltip

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ import MaterialSymbolsFavoriteRounded from '../assets/experience/MaterialSymbols
 
 const Footer = () => {
   return (
-    <div className='pt-32 p-4'>
-      <a className='grayscale has-tooltip flex justify-center' href='https://github.com/OHMelin/Portfolio' target='_blank' rel='noreferrer'>
+    <div className='pt-32 p-4 flex justify-center'>
+      <a className='grayscale has-tooltip w-fit flex justify-center' href='https://github.com/OHMelin/Portfolio' target='_blank' rel='noreferrer'>
         <span class='tooltip -mt-10 bg-secondary rounded-md p-1 px-2 justify-center'>Click to visit repository</span>
         Made with 
         <img className='inline-block ml-2' src={MaterialSymbolsFavoriteRounded} alt="" width={20} />,
